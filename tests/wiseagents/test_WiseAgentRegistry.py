86c4d33fec00a3484b994689b418f93e5590f9e6
maeste <stefano.maestri@ironjacamar.org>
2024-07-05 07:34:55 +0200

    Consider to add a shared context Fixes #28

--- tests/wiseagents/test_WiseAgentRegistry.py
+++ tests/wiseagents/test_WiseAgentRegistry.py
@@ -3,23 +3,19 @@ from wiseagents import WiseAgentContext, WiseAgentRegistry, WiseAgent
    
 def test_register_agents():
     agent = WiseAgent(name="Agent1", description="This is a test agent")
-    WiseAgentRegistry.register_agent(agent)
     assert 1 == WiseAgentRegistry.get_agents().__len__()
     assert agent == WiseAgentRegistry.get_agent(agent.name)
 
 def test_remove_agent():
-    agent = "Agent1"
-    agent = WiseAgent(name="Agent1", description="This is a test agent")
-    WiseAgentRegistry.remove_agent(agent.name)
-    assert None == WiseAgentRegistry.get_agent(agent.name)
+    agent_name = "Agent1"
+    WiseAgentRegistry.remove_agent(agent_name)
+    assert None == WiseAgentRegistry.get_agent(agent_name)
 
 def test_get_agents():
     WiseAgentRegistry.clear_agents()
     agents = [WiseAgent(name="Agent1", description="This is a test agent"), 
               WiseAgent(name="Agent2", description="This is another test agent"), 
               WiseAgent(name="Agent3", description="This is yet another test agent")]
-    for agent in agents:
-        WiseAgentRegistry.register_agent(agent)
     
     for agent in agents:
         assert agent == WiseAgentRegistry.get_agent(agent.name)
@@ -29,8 +25,6 @@ def test_get_contexts():
     contexts = [WiseAgentContext(name="Context1"), 
               WiseAgentContext(name="Context2"), 
               WiseAgentContext(name="Context3")]
-    for context in contexts:
-        WiseAgentRegistry.register_context(context)
     
     for context in contexts:
         assert context == WiseAgentRegistry.get_context(context.name)
