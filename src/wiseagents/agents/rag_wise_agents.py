9987c56204bc27f16076e53be5126c7c380e4060
Farah Juma <fjuma@redhat.com>
2024-09-04 16:11:58 -0400

    Small updates to the docstrings and constructors for the agents related to RAG
    https://github.com/wise-agents/wise-agents/issues/85

--- src/wiseagents/agents/rag_wise_agents.py
+++ src/wiseagents/agents/rag_wise_agents.py
@@ -9,38 +9,67 @@ from wiseagents.vectordb import Document, WiseAgentVectorDB
 from wiseagents import WiseAgent, WiseAgentMessage, WiseAgentTransport
 from wiseagents.llm import WiseAgentLLM
 
+"""The default number of documents to retrieve during retrieval augmented generation (RAG)."""
+DEFAULT_NUM_DOCUMENTS = 4
+
+"""The default collection name to use during retrieval augmented generation (RAG)."""
+DEFAULT_COLLECTION_NAME = "wise-agents-collection"
+
+"""The default value for whether to include the sources of the documents that were consulted to produce the response
+when using retrieval augmented generation (RAG)."""
+DEFAULT_INCLUDE_SOURCES = False
+
+"""The default number of verification questions to use when challenging the results retrieved from retrieval augmented
+generation (RAG)."""
+DEFAULT_NUM_VERIFICATION_QUESTIONS = 4
 
 class RAGWiseAgent(WiseAgent):
     """
-    This agent implementation is used to test the RAG agent.
-    Use Stomp protocol
+    This agent makes use of retrieval augmented generation (RAG) to answer questions.
     """
     yaml_tag = u'!wiseagents.RAGWiseAgent'
 
+    def __new__(cls, *args, **kwargs):
+        """Create a new instance of the class, setting default values for the optional instance variables."""
+        obj = super().__new__(cls)
+        obj._collection_name = DEFAULT_COLLECTION_NAME
+        obj._k = DEFAULT_NUM_DOCUMENTS
+        obj._include_sources = DEFAULT_INCLUDE_SOURCES
+        return obj
+
     def __init__(self, name: str, description: str, llm: WiseAgentLLM, vector_db: WiseAgentVectorDB,
-                 transport: WiseAgentTransport, collection_name: Optional[str] = "wise-agents-collection"):
-        '''Initialize the agent.
+                 transport: WiseAgentTransport, collection_name: Optional[str] = DEFAULT_COLLECTION_NAME,
+                 k: Optional[int] = DEFAULT_NUM_DOCUMENTS, include_sources: Optional[bool] = DEFAULT_INCLUDE_SOURCES):
+        """
+        Initialize the agent.
 
         Args:
             name (str): the name of the agent
             description (str): a description of the agent
-            llm (WiseAgentLLM): the LLM agent to use for processing requests
+            llm (WiseAgentLLM): the LLM to use for processing requests
             vector_db (WiseAgentVectorDB): the vector database to use for retrieving documents
             transport (WiseAgentTransport): the transport to use for communication
-            collection_name Optional(str): the name of the collection to use for retrieving documents'''
+            collection_name Optional(str): the name of the collection within the vector database to use for
+            retrieving documents, defaults to wise-agent-collection
+            k Optional(int): the number of documents to retrieve for each query, defaults to 4
+            include_sources Optional(bool): whether to include the sources of the documents that were consulted to
+            produce the response, defaults to False
+        """
         self._name = name
         self._description = description
         self._transport = transport
         self._vector_db = vector_db
         self._collection_name = collection_name
-        llm_agent = llm
-        super().__init__(name=name, description=description, transport=self.transport, llm=llm_agent,
+        self._k = k
+        self._include_sources = include_sources
+        super().__init__(name=name, description=description, transport=self.transport, llm=llm,
                          vector_db=vector_db, collection_name=collection_name)
 
     def __repr__(self):
         """Return a string representation of the agent."""
         return (f"{self.__class__.__name__}(name={self.name}, description={self.description}, llm={self.llm},"
-                f"vector_db={self.vector_db}, collection_name={self.collection_name}, transport={self.transport})")
+                f"vector_db={self.vector_db}, collection_name={self.collection_name}, transport={self.transport},"
+                f"k={self.k}, include_sources={self.include_sources})")
 
     def process_event(self, event):
         """Do nothing"""
@@ -52,13 +81,15 @@ class RAGWiseAgent(WiseAgent):
         return True
 
     def process_request(self, request: WiseAgentMessage):
-        """Process a request message by passing it to the RAG agent and sending the response back to the client.
+        """
+        Process a request message using retrieval augmented generation (RAG) and sending the response back to the client.
 
         Args:
             request (WiseAgentMessage): the request message to process
         """
-        retrieved_documents = self.vector_db.query([request.message], self.collection_name, 4)
-        llm_response_with_sources = _create_and_process_rag_prompt(retrieved_documents[0], request.message, self.llm)
+        retrieved_documents = self.vector_db.query([request.message], self.collection_name, self.k)
+        llm_response_with_sources = _create_and_process_rag_prompt(retrieved_documents[0], request.message, self.llm,
+                                                                   self.include_sources)
         self.send_response(WiseAgentMessage(llm_response_with_sources, self.name), request.sender)
         return True
 
@@ -79,37 +110,59 @@ class RAGWiseAgent(WiseAgent):
         """Get the name of the agent."""
         return self._name
 
+    @property
+    def k(self) -> int:
+        """Get the number of documents to retrieve for each query."""
+        return self._k
+
+    @property
+    def include_sources(self) -> bool:
+        """Get whether to include the sources of the documents that were consulted to produce the response."""
+        return self._include_sources
+
 class GraphRAGWiseAgent(WiseAgent):
     """
-    This agent implementation is used to test a knowledge graph based RAG agent.
-    Use Stomp protocol
+    This agent implementation makes use of Graph Retrieval Augmented Generation (Graph RAG) to answer questions.
     """
     yaml_tag = u'!wiseagents.GraphRAGWiseAgent'
+    
+    def __new__(cls, *args, **kwargs):
+        """Create a new instance of the class, setting default values for the optional instance variables."""
+        obj = super().__new__(cls)
+        obj._k = DEFAULT_NUM_DOCUMENTS
+        obj._include_sources = DEFAULT_INCLUDE_SOURCES
+        return obj
 
     def __init__(self, name: str, description: str, llm: WiseAgentLLM, graph_db: WiseAgentGraphDB,
-                 transport: WiseAgentTransport):
+                 transport: WiseAgentTransport, k: Optional[int] = DEFAULT_NUM_DOCUMENTS,
+                 include_sources: Optional[bool] = DEFAULT_INCLUDE_SOURCES):
         """
         Initialize the agent.
 
         Args:
             name (str): the name of the agent
             description (str): a description of the agent
-            llm (WiseAgentLLM): the LLM agent to use for processing requests
+            llm (WiseAgentLLM): the LLM to use for processing requests
             graph_db (WiseAgentGraphDB): the graph database to use for retrieving documents
             transport (WiseAgentTransport): the transport to use for communication
+            k Optional(int): the number of documents to retrieve for each query, defaults to 4
+            include_sources Optional(bool): whether to include the sources of the documents that were consulted to
+            produce the response, defaults to False
         """
         self._name = name
         self._description = description
         self._transport = transport
         self._graph_db = graph_db
-        llm_agent = llm
-        super().__init__(name=name, description=description, transport=self.transport, llm=llm_agent,
+        self._k = k
+        self._include_sources = include_sources
+        super().__init__(name=name, description=description, transport=self.transport, llm=llm,
                          graph_db=graph_db)
 
     def __repr__(self):
         """Return a string representation of the agent."""
         return (f"{self.__class__.__name__}(name={self.name}, description={self.description}, llm={self.llm},"
-                f"graph_db={self.graph_db}, transport={self.transport})")
+                f"graph_db={self.graph_db}, transport={self.transport}, k={self.k},"
+                f"include_sources={self.include_sources})")
 
     def process_event(self, event):
         """Do nothing"""
@@ -127,8 +180,8 @@ class GraphRAGWiseAgent(WiseAgent):
         Args:
             request (WiseAgentMessage): the request message to process
         """
-        retrieved_documents = self.graph_db.query_with_embeddings(query=request.message, k=1, retrieval_query=self._get_retrieval_query())
-        llm_response_with_sources = _create_and_process_rag_prompt(retrieved_documents, request.message, self.llm)
+        retrieved_documents = self.graph_db.query_with_embeddings(query=request.message, k=self.k, retrieval_query=self._get_retrieval_query())
+        llm_response_with_sources = _create_and_process_rag_prompt(retrieved_documents, request.message, self.llm, self.include_sources)
         self.send_response(WiseAgentMessage(llm_response_with_sources, self.name), request.sender)
         return True
 
@@ -148,6 +201,16 @@ class GraphRAGWiseAgent(WiseAgent):
     def name(self) -> str:
         """Get the name of the agent."""
         return self._name
+
+    @property
+    def k(self) -> int:
+        """Get the number of documents to retrieve for each query."""
+        return self._k
+
+    @property
+    def include_sources(self) -> bool:
+        """Get whether to include the sources of the documents that were consulted to produce the response."""
+        return self._include_sources
 
     def _get_retrieval_query(self) -> str:
         """Return the retrieval query to use for retrieving documents from the graph database."""
@@ -172,9 +235,20 @@ class CoVeChallengerRAGWiseAgent(WiseAgent):
     """
     yaml_tag = u'!wiseagents.CoVeChallengerRAGWiseAgent'
 
+    def __new__(cls, *args, **kwargs):
+        """Create a new instance of the class, setting default values for the optional instance variables."""
+        obj = super().__new__(cls)
+        obj._collection_name = DEFAULT_COLLECTION_NAME
+        obj._k = DEFAULT_NUM_DOCUMENTS
+        obj._num_verification_questions = 4
+        obj._include_sources = DEFAULT_INCLUDE_SOURCES
+        obj._num_verification_questions = DEFAULT_NUM_VERIFICATION_QUESTIONS
+        return obj
+
     def __init__(self, name: str, description: str, llm: WiseAgentLLM, vector_db: WiseAgentVectorDB,
-                 transport: WiseAgentTransport, collection_name: Optional[str] = "wise-agents-collection",
-                 k: Optional[int] = 4, num_verification_questions: Optional[int] = 4):
+                 transport: WiseAgentTransport, collection_name: Optional[str] = DEFAULT_COLLECTION_NAME,
+                 k: Optional[int] = DEFAULT_NUM_DOCUMENTS,
+                 num_verification_questions: Optional[int] = DEFAULT_NUM_VERIFICATION_QUESTIONS):
         """
         Initialize the agent.
 
@@ -184,9 +258,9 @@ class CoVeChallengerRAGWiseAgent(WiseAgent):
             llm (WiseAgentLLM): the LLM agent to use for processing requests
             vector_db (WiseAgentVectorDB): the vector database to use for retrieving documents
             transport (WiseAgentTransport): the transport to use for communication
-            collection_name Optional(str): the name of the collection to use in the vector database
-            k Optional(int): the number of documents to retrieve from the vector database
-            num_verification_questions Optional(int): the number of verification questions to generate
+            collection_name Optional(str): the name of the collection to use in the vector database, defaults to wise-agents-collection
+            k Optional(int): the number of documents to retrieve from the vector database, defaults to 4
+            num_verification_questions Optional(int): the number of verification questions to generate, defaults to 4
         """
         self._name = name
         self._description = description
@@ -290,7 +364,9 @@ class CoVeChallengerRAGWiseAgent(WiseAgent):
         llm_response = self.llm.process_single_prompt(prompt)
         return llm_response.content
 
-def _create_and_process_rag_prompt(retrieved_documents: List[Document], question: str, llm: WiseAgentLLM) -> str:
+
+def _create_and_process_rag_prompt(retrieved_documents: List[Document], question: str, llm: WiseAgentLLM,
+                                   include_sources: bool) -> str:
     """Create a RAG prompt and process it with the LLM agent.
 
     Args:
@@ -303,8 +379,10 @@ def _create_and_process_rag_prompt(retrieved_documents: List[Document], question
               f"Question: {question}\n")
     llm_response = llm.process_single_prompt(prompt)
 
-    source_documents = ""
-    for document in retrieved_documents:
-        source_documents += f"Source Document:\n    Content: {document.content}\n    Metadata: {json.dumps(document.metadata)}\n\n"
-    llm_response_with_sources = f"{llm_response.content}\n\nSource Documents:\n{source_documents}"
-    return llm_response_with_sources
+    if include_sources:
+        source_documents = ""
+        for document in retrieved_documents:
+            source_documents += f"Source Document:\n    Content: {document.content}\n    Metadata: {json.dumps(document.metadata)}\n\n"
+        return f"{llm_response.content}\n\nSource Documents:\n{source_documents}"
+    else:
+        return llm_response.content
